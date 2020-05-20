37b7efd7782562fed5e9210929e269d47e1ba5a4
Eduardo Fonseca <contact@eduu.ga>
2020-05-20 20:34:48 -0300

    More adjusts

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ var server_ip = "mc.amanoteam.com";
 
 $.ajax({url: "https://api.mcsrvstat.us/2/" + server_ip, success: function(result){
     if (result.online) {
+    	$("#server__ip").text(server_ip)
         $("#server__status").text("Server Online").addClass("on");
-        $("#server__players").html(result.players.online + " players online.");
+        $("#server__info").html(result.players.online + " players online.");
+
     }
     else {
         $("#server__status").text("Server Offline").addClass("off");
+        $("#server__info").text("Default IP:")
+        $("#server__ip").text("mc.eduu.ga")
     }
 }});
