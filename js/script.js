e81c8da901ac1e6c4c4987ccf1c4154b2ad8af13
Eduardo Fonseca <edubr029@gmail.com>
2022-11-16 09:44:29 -0300

    Change IP and server type

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 // Original Author: Alisson Lauffer <alissonvitortc@gmail.com>
 // This code is licensed under the MIT license.
 
-var server_ip = "mc.amanoteam.com";
+var server_ip = "arm.eduu.ga";
 
 function updateDetails(result){
     let status = document.getElementById("server__status");
     let info = document.getElementById("server__info");
+    let version = document.getElementById("server__version");
     let ip = document.getElementById("server__ip");
+
     if (result.online) {
         status.innerText = "Server Online";
         status.classList.add("on");
         info.innerHTML = result.players.online + " players online.";
         ip.innerText = server_ip;
+        
+        version.innerText = result.version;
     }
     else {
         status.innerText = "Server Offline";
         status.classList.add("off");
         info.innerText = "Default IP:";
         ip.innerText = "mc.eduu.ga";
+
+        version.innerText = "1.16.5";
     }
 };
 
