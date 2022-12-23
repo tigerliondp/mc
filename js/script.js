cc1f14f48083aae6d4266daa9a46df15a3f38fed
Alisson Lauffer <alissonvitortc@gmail.com>
2022-12-22 22:48:50 -0300

    major rework
    - add support for light and dark theme
    - migrate XmlHttpRequest to fetch API
    - add support for auto-updating details (every 30 seconds)
    - add support for clicking the IP button to copy to clipboard
    - minor code formatting changes

--- js/script.js
+++ js/script.js
@@ -1,43 +1,60 @@
 // Original Author: Alisson Lauffer <alissonvitortc@gmail.com>
 // This code is licensed under the MIT license.
 
-var server_ip = "arm.eduu.ga";
+let serverIP = "arm.eduu.ga";
 
-function updateDetails(result){
-    let status = document.getElementById("server__status");
-    let info = document.getElementById("server__info");
-    let version = document.getElementById("server__version");
-    let ip = document.getElementById("server__ip");
-    let motd = document.getElementById("server__motd");
+let infoElem = document.getElementById("server__info");
+let ipElem = document.getElementById("server__ip");
+let motdElem = document.getElementById("server__motd");
+let statusElem = document.getElementById("server__status");
+let versionElem = document.getElementById("server__version");
 
+ipElem.innerText = serverIP;
+
+
+let updateDetails = (result) => {
     if (result.online) {
-        status.innerText = "Server Online";
-        status.classList.add("on");
-        info.innerHTML = result.players.online + " players online.";
-        ip.innerText = server_ip;
-        
-        version.innerText = result.version;
-        motd.innerText = result.motd.clean
+        statusElem.innerText = "Server Online";
+
+        statusElem.classList.remove("off");
+        statusElem.classList.add("on");
+
+        infoElem.innerText = result.players.online + " " + (result.players.online == 1 ? "player" : "players") + " online";
+        ipElem.innerText = serverIP;
+
+        versionElem.innerText = result.version;
+        motdElem.innerText = result.motd.clean
     }
     else {
-        status.innerText = "Server Offline";
-        status.classList.add("off");
-        info.innerText = "Default IP:";
-        ip.innerText = "mc.eduu.ga";
+        statusElem.innerText = "Server Offline";
 
-        version.innerText = "1.16.5";
+        statusElem.classList.remove("on");
+        statusElem.classList.add("off");
+
+        infoElem.innerText = "Default IP:";
+        ipElem.innerText = serverIP;
+
+        versionElem.innerText = "1.16.5";
+        motdElem.innerText = "";
     }
 };
 
 
-const xhr = new XMLHttpRequest();
+ipElem.addEventListener("click", () => {
+    navigator.clipboard.writeText(serverIP);
 
-xhr.onload = () => {
-    if (xhr.status == 200) {
-        const response = JSON.parse(xhr.responseText);
-        updateDetails(response);
-    }
-};
+    ipElem.innerText = "Copied!";
+    setTimeout(() => {
+        ipElem.innerText = serverIP;
+    }, 1000);
+});
 
-xhr.open("GET", "https://api.mcsrvstat.us/2/" + server_ip);
-xhr.send();
+
+let fetchDetails = () => {
+    fetch("https://api.mcsrvstat.us/2/" + serverIP)
+        .then(response => response.json())
+        .then(data => updateDetails(data));
+}
+fetchDetails();
+
+window.setInterval(fetchDetails(), 30000);
